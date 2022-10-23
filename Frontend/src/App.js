b565d389de09a79d3e671b631b37bae9a27e07a8
Min-Qi-Zhang <minqi0303@gmail.com>
2022-10-23 17:56:48 -0400

    Removed commented code

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -12,23 +12,6 @@ const contractAddress = "0x978Fd7aF1b995373C3bCc4a3DB0d69714E561058";
 const tokenAddress = "0xef4c597986a2241D5fED9aadcb4E33a24e2D5483";
 
 class App extends Component {
-  state = {
-    // tokenAddress: ''
-  }
-  
-  componentDidMount = async () => {
-    // if (typeof window.ethereum !== 'undefined') {
-    //   const provider = new ethers.providers.Web3Provider(window.ethereum);
-    //   const contract = new ethers.Contract(contractAddress, FundManagement.abi, provider);
-    //   try {
-    //     const shareToken = await contract.getShareToken();
-    //     this.setState({ tokenAddress: shareToken });
-    //   } catch (err) {
-    //     console.log('Error: ', err);
-    //   }
-    // }
-  }
-
   requestAccount = async () => {
     await window.ethereum.request({ method: 'eth_requestAccounts' });
   };
